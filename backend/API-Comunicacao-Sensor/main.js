f3fa63949c5b4bed8fb6162cc0bbf6145408c221
gabyisv <gabrielly.campos@sptech.school>
2025-09-30 10:03:13 -0300

    Inserção com o banco de dados

--- backend/API-Comunicacao-Sensor/main.js
+++ backend/API-Comunicacao-Sensor/main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
@@ -19,11 +19,11 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: 'HOST_DO_BANCO',
-            user: 'USUARIO_DO_BANCO',
-            password: 'SENHA_DO_BANCO',
-            database: 'DATABASE_DO_BANCO',
-            port: 3306
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'sptech',
+            database: 'ccok2',
+            port: 3307
         }
     ).promise();
 
@@ -51,7 +51,7 @@ const serial = async (
     arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
         console.log(data);
         const valores = data.split(';');
-        const sensorDigital = parseInt(valores[0]);
+        const sensorDigital = parseFloat(valores[0]);
         const sensorAnalogico = parseFloat(valores[1]);
 
         // armazena os valores dos sensores nos arrays correspondentes
@@ -63,11 +63,11 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)',
-                [sensorAnalogico, sensorDigital]
+                'INSERT INTO medida ( sensor_digital) VALUES (?)',
+                [ sensorDigital]
             );
-            console.log("valores inseridos no banco: ", sensorAnalogico + ", " + sensorDigital);
-
+            console.log("valores inseridos no banco:  " + sensorDigital);
+            
         }
 
     });
