19332d6d0fbc3119f2a24b398b4835e3c2f88732
gabriellealcegatto <gabriel.cegatto@sptech.school>
2025-12-01 14:04:53 -0300

    Dash Mês Com as KPIs totalmente funcionais.

--- src/controllers/kpiController.js
+++ src/controllers/kpiController.js
@@ -1,11 +1,9 @@
 var kpiModel = require("../models/kpiModel");
 
-
-
 function buscarMaximo(req, res){
     var idEmpresa = req.body.idEmpresaServer
-
-kpiModel.buscarMaximo(idEmpresa)
+  var mes = req.body.mesServer
+kpiModel.buscarMaximo(idEmpresa,mes)
     .then(function (resultado) {
       //aqui precisa informar que o resultado vai voltar para o front em resposta JSON
       console.log("O controller recebeu o idEmpresa", idEmpresa);
@@ -17,5 +15,5 @@ kpiModel.buscarMaximo(idEmpresa)
 }
 
 module.exports = {
-    buscarMaximo,
+    buscarMaximo
 }
